d5fcc27bbb615535c7e3ca99d70de3562264739d
Ansh Jaiswal <71592135+anshjaiswal31@users.noreply.github.com>
2021-10-04 20:41:19 +0530

    ICSE 2010 CSE Solution
    LmaHoe

--- Q8.java
+++ Q8.java
@@ -1,21 +1,39 @@
+import java.util.Scanner;
 class Q8
 {
-    double series(double n)
+    public static void main(String args[])
     {
-        double sum=0;
-        for(int x=1;x<=n;x++)
+        Scanner sc=new Scanner(System.in);
+        int p[]=new int[6];
+        int q[]=new int[4];
+        int r[]=new int[10];
+        for(int x=0;x<p.length;x++)
         {
-            sum+=1.0/x;
+            System.out.println("P[]");
+            System.out.println("Enter number at index "+x);
+            p[x]=sc.nextInt();
         }
-        return sum;
-    }
-    double series(double a,double n)
-    {
-        double sum=0;
-        for(int x=1,y=1,z=2;x<=n;x++,y+=3,z+=3)
+        for(int x=0;x<q.length;x++)
         {
-            sum+=y/Math.pow(a,z);
+            System.out.println("Q[]");
+            System.out.println("Enter number at index "+x);
+            q[x]=sc.nextInt();
         }
-        return sum;
+        int i=0;
+        for(int x=0;x<p.length;x++)
+        {
+            r[i]=p[x];
+            i++;
+        }
+        for(int x=0;x<q.length;x++)
+        {
+            r[i]=q[x];
+            i++;
+        }
+        System.out.println("R[]");
+        for(int x=0;x<r.length;x++)
+        {
+            System.out.println(r[x]);
+        }
     }
 }
