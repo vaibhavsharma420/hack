e98c117c10603c065df946de633f6d6fd134ad3d
akashdsingh006 <82928605+akashdsingh006@users.noreply.github.com>
2021-10-04 22:30:36 +0530

    Sort in decreasing order.java
    Using quick sort

--- SelectionSort/decrease.java
+++ SelectionSort/decrease.java
@@ -4,30 +4,50 @@ class decrease
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int ar[]=new int[10];
-        for(int x=0;x<=9;x++)
+        int n=in.nextInt();
+        int arr[]=new int[n];
+        for(int x=0;x<n;x++)
         {
             System.out.println("Enter number at index "+(x+1));
-            ar[x]=sc.nextInt();
+            arr[x]=sc.nextInt();
         }
-        for(int x=0;x<=8;x++)
-        {
-            int index=x;
-            for(int y=x+1;y<=9;y++)
-            {
-                if(ar[y]>ar[index])
-                {
-                    index=y;
-                }
-            }
-            int t=ar[x];
-            ar[x]=ar[index];
-            ar[index]=t;
-        }
+        ob.sorting(arr[],0,n-1);
         System.out.println("Sorted Array:");
         for(int x=0;x<=9;x++)
         {
             System.out.println(ar[x]);
         }
+    }
+}
+ public int partition(int arr[], int left, int right)
+    {
+       int pivot = arr[right];
+       int i = left - 1;
+       for(int j = right; j >= left; j--)
+    {
+        if (arr[j] > pivot)
+        {
+            i = i + 1;                                      
+            int temp = arr[i];
+            arr[i]= arr[j];
+            arr[j]= temp;
+        }
+    }
+
+    int temp = arr[i+1];
+    arr[i+1] = arr[right];
+    arr[right] = temp;
+
+    return i + 1;
+
+    }
+
+public void sorting(int arr[], int left, int right)
+{
+    if(left < right)
+    {
+        int q = partition(arr, left, right);
+        sorting(arr, left, q - 1);
+        sorting(arr, q + 1, right);
     }
 }
