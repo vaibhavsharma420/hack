74201bcb4f84a88e3968bbd4fcc8d689c795a306
Kaustubh Jaiswal <kaustubhjaiswal200@gmail.com>
2021-10-23 22:55:45 +0530

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,6 @@
+export { default as Main } from "./Main";
+export { default as KryptoDetails } from "./KryptoDetails";
+export { default as Footer } from "./Footer";
+export { default as Navbar } from "./Navbar";
+export { default as Loader } from "./Loader";
+export { default as LineChart } from "./LineChart";
